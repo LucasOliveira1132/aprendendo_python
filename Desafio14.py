fb64be93c49e64d53cdf651a0f6ad5312b6883ea
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-03-25 14:24:52 -0300

    Conversor de Temperatura

---
+++ Desafio14.py
@@ -0,0 +1,3 @@
+temperatura_c = float(input('Informe a Temperatura em °C: '))
+temperatura_f = (temperatura_c*9)/5+32
+print(f'A Temperatura De {temperatura_c:.1f}°C, Corresponde a {temperatura_f:.1f}°F')
