aed4b2ecc39570d370798c4511a1ebcad807ae11
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-03-25 15:48:04 -0300

    Reajuste Salarial

--- Desafio13.py
+++ Desafio13.py
@@ -2,3 +2,4 @@ valor = float(input('Qual o Salário Do Funcionário?'))
 aumento = valor *15/100
 total = aumento + valor 
 print(f'Um Funcionário Que Ganhava {valor}, Com Um Aumento De 15%, Passa a Receber {total:.2f}')
+#Reajuster Salarial
