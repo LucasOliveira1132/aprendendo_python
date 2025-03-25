439ba6bfcc7500d9e36bb5e143e920e54f0fe6aa
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-03-25 15:37:49 -0300

    dissecar uma variáve

--- Desafio04.py
+++ Desafio04.py
@@ -4,3 +4,4 @@ print(f'O tipo Primitivo Desse valor é {(type(n))}')
 print(f'So tem espaço?, {n.isspace()}')
 print(f'é um numero?, {n.isnumeric()}')
 print(f'é Alfabetico?, {n.isalpha()}')
+# dissecar uma variável
