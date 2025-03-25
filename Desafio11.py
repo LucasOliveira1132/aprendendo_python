0727427f61ace4274db564822a227e35d9dcc78c
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-03-25 15:45:33 -0300

    Pintando Paredes

--- Desafio11.py
+++ Desafio11.py
@@ -3,3 +3,4 @@ a = float(input('Qual a Altura Da Parede (Em Metros):'))
 area = l*a 
 tinta = area/2
 print(f'Sua Parede Tem uma Area de {area}M, Seria necessario {tinta} Litros De Tinta Para Pintar A Parede')
+#Pintando Paredes
