79e0bd248447d5b6c5497b37a236e3cf7c7a57fb
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-03-28 22:35:34 -0300

    Seno, Cosseno e Tangente

---
+++ Desafio18.py
@@ -0,0 +1,8 @@
+import math
+angulo = float(input('Digite um angulo: '))
+seno = math.sin(math.radians(angulo))
+print(f'O ângulo de {angulo} Tem o SENO De {seno:.2f}')
+cosseno = math.cos (math.radians(angulo))
+print(f'O ângulo De {angulo} Tem o COSSENO De {cosseno:.2f}')
+tangente = math.tan(math.radians(angulo))
+print(f'O ângulo de ){angulo}) Tem a TanGente De {tangente:.2f}')
