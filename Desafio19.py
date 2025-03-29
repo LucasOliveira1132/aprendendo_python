d5a8f7ac8ff7f329c91d18e60c5a4ed184c450a0
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-03-29 11:29:19 -0300

    Sorteando Um Nome em uma lista

--- Desafio19.py
+++ Desafio19.py
@@ -1,3 +1,4 @@
+'''
 import random 
 nome1 = str(input('Digite um Nome:'))
 nome2 = str(input('Digite Mais Um Nome:'))
@@ -5,3 +6,14 @@ nome3 = str(input('Digite Mais Um Nome:'))
 nome4 = str(input('Digite Mais um Nome:'))
 sortudo = random.choice([nome1, nome2, nome3, nome4])
 print(f'A Pessoa Escolhida Foi {sortudo}')
+'''
+
+from random import choice
+#choice serve Para Sortear um item de uma lista
+nome1 = str(input('Digite um Nome:'))
+nome2 = str(input('Digite um Nome:'))
+nome3 = str(input('Digite um Nome:'))
+nome4 = str(input('Digite um Nome:'))
+lista = [nome1, nome2, nome3, nome4]
+escolhido = choice(lista)
+print(f'o Escolhido Foi {escolhido}')
