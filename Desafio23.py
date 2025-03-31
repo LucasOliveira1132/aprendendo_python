c4a4b5ab1707f6c65f8578df7973e300b802b651
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-03-31 19:07:46 -0300

    Separando dígitos de um número

---
+++ Desafio23.py
@@ -0,0 +1,6 @@
+numero = str(input('Digite um Valor de 0 a 9999: '))
+print(f'Unidade: {numero[3:4]}')
+print(f'Dezena: {numero[2:3]}')
+print(f'Centena: {numero[1:2]}')
+print(f'Milhar {numero[0:1]}')
+#Separando dígitos de um número
