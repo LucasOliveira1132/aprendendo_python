e5560c537edb1abfbeda516cc9489ebc589e7785
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-04-01 23:25:35 -0300

    Primeiro e último nome de uma pessoa

---
+++ Desafio27.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite seu nome completo: ')).strip().split()
+print(f'O seu primeiro Nome é: {nome[0]}')
+print(f'O seu último Nome é: {nome[-1]}')
+
+#Primeiro e último nome de uma pessoa
