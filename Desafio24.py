2602634f7f6b0d30ae9b3dd9cfb9f360588a3e28
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-04-01 23:19:52 -0300

    Verificando as primeiras letras de um texto

--- Desafio24.py
+++ Desafio24.py
@@ -1,5 +1,5 @@
-cidade = str(input('Diga o Nome da Cidade Que Você Mora:'))
-cidade.lower()
-print(f'Sua Cidade Começa com Santo? {cidade.lower().startswith("santo")}')
+cidade = str(input('Diga o Nome da Cidade Que Você Mora:')).lower().strip()
+#cidade.lower()
+print(f'Sua Cidade Começa com Santo? {cidade.startswith("santo")}')
 #O startswith()método retorna True se a string começar com o valor especificado, caso contrário, False.
 #Verificando as primeiras letras de um texto
