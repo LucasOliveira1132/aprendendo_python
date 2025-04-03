beb5609af04ec17b5948b012a6022e2a6890cea8
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-04-03 01:20:54 -0300

    Maior e menor valores

---
+++ Desafio33.py
@@ -0,0 +1,19 @@
+valor1 = int(input('Primeiro valor: '))
+valor2 = int(input('Segundo Valor: '))
+valor3 = int(input('Terceiro Valor: '))
+
+menor = valor1
+if valor2 < valor1 and valor2 < valor3:
+    menor = valor2
+if valor3 < valor1 and valor3 < valor2:
+    menor = valor3
+
+maior = valor1
+if valor2 > valor1 and valor2 > valor3:
+    maior = valor2
+if valor3 > valor1 and valor3 > valor2:
+    maior = valor3
+print(f'O menor valor digitado foi {menor}')
+print(f'O maior valor digitado foi {maior}')
+
+#Maior e menor valores
