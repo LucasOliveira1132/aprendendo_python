ce661600fd630f277c8f2c41fd4e5226fa532f15
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-04-03 01:19:10 -0300

    Custo da Viagem

---
+++ Desafio31.py
@@ -0,0 +1,7 @@
+km = float(input('Quantos KM vai dar a sua viagem ?'))
+if km <= 200:
+    print(f'Sua Viagem ficou R$ {km*0.50}')
+else:
+    print(f'Sua Viagem ficou R$ {km*0.45}')
+
+#Custo da Viagem
