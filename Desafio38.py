33d36103968f9822d2e7850c72b90e7cd24545d1
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-04-11 20:30:25 -0300

    Comparando números

---
+++ Desafio38.py
@@ -0,0 +1,9 @@
+numero1 = int(input('Digite um número inteiro: '))
+numero2 = int(input('Digite Mais um número inteiro: '))
+if numero1 > numero2:
+    print(f'O {numero1} é maior do que o {numero2}')
+elif numero2 > numero1:
+    print(f'O {numero2} é maior do que o {numero1}')
+else:
+    print('Os dois números é igual')
+    #Comparando números
