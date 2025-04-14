4cbc90dc2b7e2ce84c600d2beb1a72e54b13e2ea
Lucas Oliveira <lucasdeoliveiradasilva58@gmail.com>
2025-04-14 20:35:11 -0300

    Tabuada v.2.0

---
+++ Desafio49.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um Número que você quer saber a Tabuada:'))
+tabuada = 0
+for contador in range(0, 10):
+    tabuada += 1
+    print(f'{numero}x{tabuada} = {numero * tabuada} ')
+
+#Tabuada v.2.0
